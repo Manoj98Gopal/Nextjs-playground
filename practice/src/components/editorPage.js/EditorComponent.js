19e7c20fd222cd1de6ec981cce783ff201ca02af
manoj98gopal <manoj98gopal@gmail.com>
2024-03-28 17:00:25 +0530

    mui and editorjs installed

---
+++ practice/src/components/editorPage.js/EditorComponent.js
@@ -0,0 +1,25 @@
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
+
+export const EditorComponent = () => {
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ padding: "20px" }}>
+        <Box
+          sx={{
+            border: "1px solid black",
+            borderRadius: "15px",
+            height: "89vh",
+            padding: "20px",
+          }}
+        >
+          <Typography variant="h5" textAlign="center" pb={2}>
+            Text Editor
+          </Typography>
+
+
+        </Box>
+      </Container>
+    </>
+  );
+};
